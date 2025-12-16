a88f2722ff193ee2fd2a96e229b0202a1a711bbf
wattsryaneric <wattsryaneric@icloud.com>
2025-12-16 21:56:07 +0300

    Move 'use client' to top of page.tsx

--- eventbridgeapp/app/(main)/page.tsx
+++ eventbridgeapp/app/(main)/page.tsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic'; 
 'use client'; 
+
+export const dynamic = 'force-dynamic'; 
 
 import HeroSection from '@/components/home/hero-section';
 import BookingProcess from '@/components/home/booking-process';
