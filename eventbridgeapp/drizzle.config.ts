6a8932a076b4eeba926be8fdc724d260d635a202
wattsryaneric <wattsryaneric@icloud.com>
2025-12-26 14:19:05 +0300

    remove error

--- eventbridgeapp/drizzle.config.ts
+++ eventbridgeapp/drizzle.config.ts
@@ -1,10 +1,5 @@
 // drizzle.config.ts
 import type { Config } from 'drizzle-kit';
-import { loadEnvConfig } from '@next/env';
-
-// Load environment variables
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
 
 export default {
   schema: './drizzle/schema.ts',
