66a87046cbb2bda6f6ffe8a165d817b61b1b29ea
MUGOLE JOEL <joelmugole74@gmail.com>
2026-01-04 15:17:21 +0300

    the new

--- eventbridgeapp/components/vendor/dashboard/bookinfo.tsx
+++ eventbridgeapp/components/vendor/dashboard/bookinfo.tsx
@@ -1,7 +1,7 @@
 export default function BookInfo() {
     return (
         <section aria-label="Book info">
-            <h1 className="font-font1 font-semibold text-[14px] leading-[20px] tracking-normal text-white">Book info</h1>
+            <h1 className="font-font1 font-semibold text-[14px] leading-5 tracking-normal text-white">Book info</h1>
         </section>
     );
 }
